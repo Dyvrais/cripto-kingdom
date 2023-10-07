8b16f8b1d1b65da749bec5984b2e3807bb4d1d05
Deyver Reyes Faria <78991597+Dyvrais@users.noreply.github.com>
2023-10-07 19:22:12 -0400

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ export default function Header() {
       <nav className="navbar z-10 flex bg-[#25093b] justify-around p-2 fixed top-0 w-[100%]">
         <div className="logo flex">
           <img
-            src="./src/assets/cripto-kingdom-logo.png"
+            src="../src/assets/cripto-kingdom-logo.png"
             className="w-7 h-7 m-auto ml-2"
           />
           <h2 className="text-slate-100 m-auto invisible lg:visible lg:pl-3">
